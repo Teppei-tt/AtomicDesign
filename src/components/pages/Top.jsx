b54d22ec4ff58645938967df8d5b4ef804982e51
Teppei-tt <77786751+Teppei-tt@users.noreply.github.com>
2021-08-11 22:02:43 +0900

    initial commit

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,8 +1,17 @@
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+
 export const Top = () => {
+  const history = useHistory();
+  const onClickAdmin = () => console.log("a");
+  const onClickGeneral = () => console.log("b");
+
   return (
     <SContainer>
       <h2>TOP PAGE</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
