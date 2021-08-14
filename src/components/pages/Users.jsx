0c4926d161cd1e23ae084c392686bce362916032
Teppei-tt <77786751+Teppei-tt@users.noreply.github.com>
2021-08-14 21:20:10 +0900

    Initial　Comit ブランチ残したいので変更

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,7 +1,7 @@
-import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
+
 const users = [...Array(10).keys()].map((val) => {
   return {
     id: val,
@@ -17,16 +17,13 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { state } = useLocation();
-  const isAdmin = state ? state.isAdmin : false;
-
   return (
     <SContainer>
       <h2>USERS</h2>
       <SearchInput />
       <SUserArea>
-        {users.map((user) => (
-          <UserCard key={user.id} user={user} isAdmin={isAdmin} />
+        {users.map((obj) => (
+          <UserCard key={obj.id} user={obj} />
         ))}
       </SUserArea>
     </SContainer>
